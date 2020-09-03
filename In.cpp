cc4b2d34125f47c9c3d7d847db96c49085415438
zheniacherniavsky <zheniacherniavskyplay@gmail.com>
2020-09-04 01:13:12 +0300

    in file

---
+++ In.cpp
@@ -0,0 +1,148 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+// REGULAR HANDLER
+// you can enable or disable the regular express handler
+// false:
+//		in -in you get only handled program text
+// true:
+//		you enable regular express handler
+#define REGULAR_HANDLER false 
+
+#include "In.h"
+#include "Error.h"
+#include "Log.h"
+#include "FST/FST.h"
+#include <fstream>
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+namespace In {
+
+	int getAscii(char a) {
+		for (int i = 0; i < 256; i++)
+			if ((char)i == a) return i;
+		throw ERROR_THROW(0);
+	}
+
+	_IN_ getin(char* dir, char* outdir) {
+		_IN_ info;
+		info.fullsize = 0;
+		info.size = 0;
+		info.text = new char[IN_MAX_LEN_TEXT];
+		info.lines = 0;
+		info.fullLines = 0;
+		info.ignored = 0;
+
+		ifstream text(dir);
+		char buff[IN_MAX_LEN_LINE];
+		if(!text.is_open()) throw ERROR_THROW(0);
+		while (!text.eof()) {
+			text.getline(buff, IN_MAX_LEN_LINE);
+			info.lines++;
+			for (int i = 0; i <= strlen(buff); i++) {
+				info.fullsize++;
+				// debug check
+				std::cout << buff[i] << " " << i << " code: " << getAscii(buff[i]) << std::endl;
+
+				// check spaces
+				if (getAscii(buff[i]) == 32 && getAscii(buff[i + 1] == 32)) {
+					i--;
+					continue;
+				} else if ((getAscii(buff[i]) == 32 && i == 0))
+					continue;
+
+				if (IN_CODE_TABLE[getAscii(buff[i])] == T) {
+					info.text[info.size] = buff[i];
+					info.size++;
+				}
+
+				else if (IN_CODE_TABLE[getAscii(buff[i])] == F) {
+					throw ERROR_THROW_IN(110, info.lines, i + 1);
+				}
+
+				else if (IN_CODE_TABLE[getAscii(buff[i])] == I) {
+					info.ignored++;
+					continue;
+				}
+
+				else {
+					info.text[info.size] = (char)IN_CODE_TABLE[getAscii(buff[i])];
+					info.size++;
+				}
+			}
+			info.fullLines++;
+		}
+
+		info.text[info.size] = '\0';
+		cout << info.text << endl;
+		text.close();
+
+		// OUT
+
+		// a(g)⁺((b+c+d)e(g)⁺)*g⁺fe - regular expression
+
+		// a - main
+		// b - send
+		// c - wait
+		// d - show
+		// e - ;
+		// f - return
+		// g - SPACE (' ')
+
+		if (REGULAR_HANDLER) {
+			ofstream out(outdir);
+			char* regular = strtok(info.text, "\n");
+
+			while (regular != NULL) {
+				FST::FST fst1(
+					regular,
+					9,
+					FST::NODE(1, FST::RELATION('a', 1)),
+					FST::NODE(2, FST::RELATION('g', 2), FST::RELATION('g', 1)),
+
+					FST::NODE(5,
+						FST::RELATION('b', 3),
+						FST::RELATION('c', 3),
+						FST::RELATION('d', 3),
+						FST::RELATION('g', 6),
+						FST::RELATION('g', 5)
+					),
+					FST::NODE(1, FST::RELATION('e', 4)),
+
+					FST::NODE(5,
+						FST::RELATION('g', 5),
+						FST::RELATION('g', 4),
+						FST::RELATION('b', 3),
+						FST::RELATION('c', 3),
+						FST::RELATION('d', 3)
+					),
+
+					FST::NODE(2, FST::RELATION('g', 6), FST::RELATION('g', 5)),
+
+					FST::NODE(1, FST::RELATION('f', 7)),
+					FST::NODE(1, FST::RELATION('e', 8)),
+
+					FST::NODE()
+				);
+				if (FST::execute(fst1))
+				{
+					out << "(+) Цепочка распознана:\t\t\t" << regular << "\n";
+				}
+				else
+				{
+					out << "(-) Цепочка не распознана:\t\t" << regular << "\n";
+				}
+				regular = strtok(NULL, "\n");
+			}
+			out.close();
+		}
+		else {
+			ofstream out(outdir);
+			out << info.text;
+			out.close();
+		}
+
+		return info;
+	}
+}
---
+++ In.h
@@ -0,0 +1,56 @@
+﻿#pragma once
+#define IN_MAX_LEN_TEXT 1024*1024
+#define IN_MAX_LEN_LINE 256
+#define IN_CODE_ENDL '\n'
+
+namespace In {
+	enum { T = 1024, F = 2048, I = 4096 };
+	struct _IN_ {
+		int fullsize;
+		int size;
+		int lines;
+		int fullLines;
+		int ignored;
+		char* text;
+	};
+
+	// таблица проверки входной информации, индекс = код (W1251) символа
+	// F - запрещенный символ, T - разрешенный символ, I - игнорировать (не вводить),
+	//		если 0 <= значение < 256 - то вводится данное значение
+	const unsigned short IN_CODE_TABLE[256] = {
+	// 0     1      2      3      4      5      6      7      8      9     10     11     12     13     14      15
+	'\n',  In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, // 1
+	// 16
+	In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, // 2
+	// 32
+	In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, // 3
+	// 48
+	In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, // 4
+	// 64
+	In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, // 5
+	// 80
+	In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, // 6
+	// 96
+	In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, // 7
+	// 112
+	In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, // 8
+	// 128
+	In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, // 9
+	// 144
+	In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, // 10
+	// 160
+	In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, // 11
+	// 176
+	In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, // 12
+	// 192
+	In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, // 13
+	// 208
+	In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, // 14
+	// 224
+	In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, // 15
+	// 240
+	In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T, In::T  // 16
+	};
+
+	_IN_ getin(char* dir, char* outdir);
+};
