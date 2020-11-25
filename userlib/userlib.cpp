8007b9edd3169ab1935798adfc0898e1398ae044
zheniacherniavsky <zheniacherniavskyplay@gmail.com>
2020-11-25 16:18:32 +0300

    outint function

---
+++ userlib/userlib.cpp
@@ -0,0 +1,15 @@
+﻿// userlib.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: Это пример библиотечной функции.
+extern "C"
+{
+	void __stdcall outint(int a)
+	{
+		std::cout << a << std::endl;
+		return;
+	}
+}
